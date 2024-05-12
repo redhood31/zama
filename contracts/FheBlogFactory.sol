d0176ece81553e3295a5ebf141080cd01aa3063c
Maksym Zub <kroistsmeh@gmail.com>
2024-05-12 13:59:19 +0200

    upd

--- contracts/FheBlogFactory.sol
+++ contracts/FheBlogFactory.sol
@@ -23,7 +23,7 @@ contract FHEBlogFactory is EIP712WithModifier {
         return Clones.predictDeterministicAddress(implementation, salt);
     }
    
-    function createBlog(BlogStorage memory _data, string memory _nft_name, string memory _nft_short_name, bytes32 salt) public{
+    function createBlog(BlogStorage calldata _data, string calldata _nft_name, string calldata _nft_short_name, bytes32 salt) public{
         address cloneAdd = Clones.cloneDeterministic(implementation,salt);
         FHE_BLOG(cloneAdd).initialize(
             _data,
@@ -33,8 +33,15 @@ contract FHEBlogFactory is EIP712WithModifier {
         blogs[blogsCount] = cloneAdd;
         blogsCount++;
     }       
-    function createBlog(BlogStorage memory _data, bytes32 salt) public{
-        createBlog(_data,  'FHE_BLOG', 'FHBL', salt);
+    function createBlog(BlogStorage calldata _data, bytes32 salt) public{
+        address cloneAdd = Clones.cloneDeterministic(implementation,salt);
+        FHE_BLOG(cloneAdd).initialize(
+            _data,
+            'FHE_BLOG',
+            'FHBL'
+        );
+        blogs[blogsCount] = cloneAdd;
+        blogsCount++;
     }
 
 
